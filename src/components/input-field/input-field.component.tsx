9b43e57cc29f574ad8d5df257a756b563ec40075
ipaullly <ipaully22@gmail.com>
2020-02-11 17:25:30 +0300

    Implement bus search page

---
+++ src/components/input-field/input-field.component.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './input-field.styles.css';
+
+interface FormProps {
+  label: string;
+  name: string;
+  value: string | number;
+  handleChange: (e: React.FormEvent<HTMLInputElement>) => any;
+  type: string;
+}
+
+const FormInput: React.FC<FormProps> = ({ handleChange, label, ...otherProps }) => (
+  <div className="group">
+    <input className="form-input" onChange={handleChange} {...otherProps} />
+    {label ? (
+      <label
+        className={`
+        ${otherProps.value ? 'shrink' : ''}
+        form-input-label`}
+      >
+      {label}
+      </label>
+    ) : null
+    }
+  </div>
+)
+
+export default FormInput;
---
+++ src/pages/home-page/home-page.component.tsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './home.styles.css';
+import FormInput from 'src/components/input-field/input-field.component';
+
+interface SearchState {
+  travellingFrom: string,
+  travellingTo: string,
+  departureDate: number
+}
+
+class HomePage extends Component<{}, SearchState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      travellingFrom: '',
+      travellingTo: '',
+      departureDate: 0,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (field: string) => (e: any) => {
+    const value = e.currentTarget.value;
+    this.setState({ [field]: value } as SearchState);
+  }
+
+  render() {
+    const { travellingFrom, travellingTo, departureDate } = this.state;
+    return(
+      <div className="container">
+        <h2 className="header">Search for Available Buses</h2>
+        <div className="search-fields">
+          <FormInput 
+            name="travellingFrom"
+            type="text"
+            value={travellingFrom}
+            handleChange={this.handleChange('travellingFrom')}
+            label="Travelling From"
+          />
+          <FormInput 
+            name="travellingTo"
+            type="text"
+            value={travellingTo}
+            handleChange={this.handleChange('travellingTo')}
+            label="Destination"
+          />
+          <FormInput 
+            name="departureDate"
+            type="date"
+            value={departureDate}
+            handleChange={this.handleChange('departureDate')}
+            label="Date of Departure"
+          />
+          <button>Search</button>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default HomePage;
