90ad13e05de904980b57f1ad100441698c73532c
ipaullly <ipaully22@gmail.com>
2020-02-18 09:55:16 +0300

    change home component to stateless component

--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -14,54 +14,42 @@ interface SearchState {
   obj: Options | null;
 }
 
-class HomePage extends Component<{}, SearchState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      obj: null,
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+const HomePage: React.FC<{}> = () => {
+  
 
-  handleChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
-    this.setState({ obj: Cities.cities[parseInt(e.target.value)] });
-    console.log(this.state);
-    
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    //
   }
-
-  render() {
-    return(
-      <div className="container">
-        <h2 className="header">Search for Available Buses</h2>
-        <hr/>
-        <p className="intro">Find the most convenient and affordable bus fares here!</p>
-        <div className="search-fields">
-          <select className="form-control" onBlur={this.handleChange}>
-            <option>---travelling-from---</option>
-            {
-              Cities.cities && Cities.cities.map((e, key) => {
-              return <option key={key} value={e.Key}>{e.Value}</option>;
-              })
-            }
-          </select>
-          <select className="form-control" onBlur={this.handleChange}>
-            <option>---travelling-to---</option>
-            {
-              Cities.cities && Cities.cities.map((e, key) => {
-              return <option key={key} value={e.Key}>{e.Value}</option>;
-              })
-            }
-          </select>
-          <input type="date" placeholder="depature date" className="form-control date-time"/>
-          <CustomButton type='submit'><Link to='/trips' className="link">Search Trips</Link></CustomButton>
-        </div>
-        <div className="mpesa">
-          You can pay via M-pesa
-        </div>
+  return(
+    <div className="container">
+      <h2 className="header">Search for Available Buses</h2>
+      <hr/>
+      <p className="intro">Find the most convenient and affordable bus fares here!</p>
+      <div className="search-fields">
+        <select className="form-control" onBlur={handleChange}>
+          <option>---travelling-from---</option>
+          {
+            Cities.cities && Cities.cities.map((e, key) => {
+            return <option key={key} value={e.Key}>{e.Value}</option>;
+            })
+          }
+        </select>
+        <select className="form-control" onBlur={handleChange}>
+          <option>---travelling-to---</option>
+          {
+            Cities.cities && Cities.cities.map((e, key) => {
+            return <option key={key} value={e.Key}>{e.Value}</option>;
+            })
+          }
+        </select>
+        <input type="date" placeholder="depature date" className="form-control date-time"/>
+        <CustomButton type='submit'><Link to='/trips' className="link">Search Trips</Link></CustomButton>
       </div>
-    );
-  }
-
+      <div className="mpesa">
+        You can pay via M-pesa
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
